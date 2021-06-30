9ab3276565276a4157bbc200613e7f2069724e13
claudiuri <claudio.y07@gmail.com>
2021-06-30 18:13:29 -0300

    feat: ensure SingUpController returns 400 if no name provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from './signup'
+describe('SingUp Controller', () => {
+  test('Schould return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfimation: 'any_password'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+  })
+})
