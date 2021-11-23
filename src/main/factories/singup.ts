fc89d61cf260869ca3b02d1e57a14a63c48de075
claudiuri <claudio.y07@gmail.com>
2021-11-22 21:05:02 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adpter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/singup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdpter } from '../../utils/email-validator-adapter'
@@ -12,5 +13,6 @@ export const makeSignUpController = (): Controller => {
   const aAccountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, aAccountMongoRepository)
   const singUpController = new SignUpController(emailValidatorAdpter, dbAddAccount)
-  return new LogControllerDecorator(singUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(singUpController, logMongoRepository)
 }
